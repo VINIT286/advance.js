9ab6d315ffb3a839d6684e5cd3b0831825886e94
VINIT286 <vinitgandhe10301@gmail.com>
2025-04-17 13:18:06 +0530

    files add

--- reg.js
+++ reg.js
@@ -18,8 +18,6 @@ async function vg() {
         },
         body:JSON.stringify(obj)
     })
-
-    console.log(b)
 }
 
 async function del(arg) {
@@ -29,9 +27,47 @@ async function del(arg) {
     
 }
 
+function final(id){
+    alert()
+    let sname = document.querySelector('#sname').value;
+    let email = document.querySelector('#email').value;
+    let mobile = document.querySelector('#mobile').value;
+    let pass = document.querySelector('#pass').value;
+    let cpass = document.querySelector('#cpass').value;
+    let obj={
+        "sname": sname,
+        "email": email,
+        "mobile": mobile,
+        "pass": pass, 
+        "cpass": cpass 
+    }
+    fetch(`http://localhost:4000/registration/${id}`,{
+        method:"PUT",
+        headers:{
+            "content-type":"application/json",
+        },
+        body:JSON.stringify(obj)
+    });
+}
+
+async function up(id) {
+    let reg = await fetch(`http://localhost:4000/registration/${id}`);
+    let res = await reg.json();
+    let ui = `<form>
+    <input type="text" id="sname" value="${res.sname}">
+    <input type="text" id="email" value="${res.email}">
+    <input type="text" id="mobile" value="${res.mobile}">
+    <input type="text" id="pass" value="${res.pass}">
+    <input type="text" id="cpass" value="${res.cpass}">
+    <button onclick="final('${id}')">update </button>
+    </form>`
+    let fo = document.querySelector('#form');
+    fo.innerHTML=ui
+}
+
 async function f(){
-    let data = await fetch('http://localhost:4000/registration');
-    let res = await data.json();
+    let reg = await fetch('http://localhost:4000/registration');
+    let res = await reg.json();
     let o=document.querySelector('#output')
     o.innerHTML=res.map(e=>`
         <div>${e.sname}</div>
@@ -39,6 +75,6 @@ async function f(){
         <div>${e.mobile}</div>
         <div>${e.pass}</div>
         <div>${e.cpass}</div>
-        <button onclick="del('${e.id}')">remove </button>`).join(" ")
-    
+        <button onclick="del('${e.id}')">remove </button>
+        <button onclick="up('${e.id}')">update</button>`).join(" ")
 }
